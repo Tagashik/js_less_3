03792206339405457c791b0bb181e7c308a493ea
Tagashik <tagashuk@gmail.com>
2021-09-06 18:34:00 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -100,6 +100,7 @@ let myPow = (a, b, callback) => {
  * car.year – год выпуска, число
  * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
  *
+ * 
  * #5
  *
  * Для созданных ранее объектов определите метод info(), используя ключевое слово this.
@@ -118,24 +119,48 @@ let myPow = (a, b, callback) => {
  * - необходимо изменить год выпуска автомобиля, установив в качестве значения текущий год
  * - если сеттеру used присвоено значение 'used', ничего делать не нужно
  */
+function fullInfo() {
+  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+}
+
+let yearNow = new Date().getFullYear(2021);
+let car = {
+  engine: 2000,
+  model: 'Lacetti',
+  name: 'Chevrolet',
+  year: 2010,
+  info: fullInfo,
+  get used() {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) this.year = yearNow;
+  }
+};
+let car2 = {
+  engine: 5000,
+  model: 'FX50 AWD',
+  name: 'Infinite',
+  year: 2019,
+  info: fullInfo,
+  get used() {
+    return yearNow - this.year ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) this.year = yearNow;
+  }
+};
 
 // let yearNow = new Date().getFullYear(); // получить текущий год как число
-
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
-
-// car.used = 'new';
-
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
-
-// car.used = 'used';
-
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
-
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
-
-// car.used = 'used';
-
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
+console.log(yearNow);
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
+car.used = 'new';
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
+car.used = 'used';
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
+console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
+car.used = 'used';
+console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
 
 /*
  * #7
@@ -144,35 +169,37 @@ let myPow = (a, b, callback) => {
  * В реализации функции должен быть применен метод Math.max() и apply().
  */
 
-// let list = [12, 23, 100, 34, 56, 9, 233];
-
-// console.log(myMax(list)); // 233
+let list = [12, 23, 100, 34, 56, 9, 233];
+let myMax = (arr) => Math.max.apply(Math, arr);
+console.log(myMax(list)); // 233
 
 /*
  * #8
  *
  * Создайте функцию myMul(a, b), которая будет умножать числа а и b, возвращая результат.
  */
-
+function myMul(a, b) {
+  return a * b;
+}
 /*
  * создайте функции myDouble(n), которая принимает один параметр и  удваивает его.
  * Использовать умножение или другие математические операции внутри функции – запрещено, только bind() и myMul().
  * Функция возвращает результат вычисления.
  */
+let myDouble = myMul.bind(null, 2);
+console.log(myDouble(3)); // = myMul(2, 3) = 6
 
-// console.log(myDouble(3)); // = myMul(2, 3) = 6
+console.log(myDouble(4)); // = myMul(2, 4) = 8
 
-// console.log(myDouble(4)); // = myMul(2, 4) = 8
-
-// console.log(myDouble(5)); // = myMul(2, 5) = 10
+console.log(myDouble(5)); // = myMul(2, 5) = 10
 
 // аналогичным образом создайте функцию myTriple(n), которая утраивает принимающий параметр, возвращая результат.
+let myTriple = myMul.bind(null, 3);
+console.log(myTriple(3)); // = myMul(3, 3) = 9
 
-// console.log(myTriple(3)); // = myMul(3, 3) = 9
+console.log(myTriple(4)); // = myMul(3, 4) = 12
 
-// console.log(myTriple(4)); // = myMul(3, 4) = 12
-
-// console.log(myTriple(5)); // = myMul(3, 5) = 15
+console.log(myTriple(5)); // = myMul(3, 5) = 15
 
 /*
  * #9
@@ -184,10 +211,17 @@ let myPow = (a, b, callback) => {
  * Любые условные операторы – запрещены и объекты.
  */
 
-// let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
 
-// let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
+let myUniq = (arr) => {
+  let set = new Set();
+  arr.forEach((val) => {
+    set.add(val);
+  });
 
-// console.log(myUniq(notUniqNums));
+  return set;
+};
+console.log(myUniq(notUniqNums));
+console.log(myUniq(notUniqStrings));
 
-// console.log(myUniq(notUniqStrings));
